c7be36d564fa8b11a9bcce89ac685331fda7d325
José Ramírez <jramirez@develsystems.com>
2021-11-30 16:41:00 -0600

    feat: add shared widgets for dynamism

--- lib/Place/ui/screens/add_place_screen.dart
+++ lib/Place/ui/screens/add_place_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:platzi_trip_app/Place/ui/widgets/gradient_back.dart';
+import 'package:platzi_trip_app/Shared/ui/widgets/text_input.dart';
+import 'package:platzi_trip_app/Shared/ui/widgets/title_header.dart';
 
 class AddPlaceScreen extends StatefulWidget{
   File image;
@@ -14,6 +16,10 @@ class AddPlaceScreen extends StatefulWidget{
 }
 
 class _AddPlaceScreen extends State<AddPlaceScreen>{
+
+  final _controllerTitlePlace = TextEditingController();
+  final _controllerDescriptionPlace = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,8 +42,42 @@ class _AddPlaceScreen extends State<AddPlaceScreen>{
                     },
                   ),
                 ),
-              )
+              ),
+              Flexible(
+                child: Container(
+                  padding: EdgeInsets.only(top:45.0, left: 10.0),
+                  child: TitleHeader(title: 'Add a new place'),
+                )
+              ),
             ],
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 120.0, bottom: 20.0),
+            child: ListView(
+              children: [
+                Container(
+
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: TextInput(
+                    hintText: 'Title',
+                    inputType: null,
+                    maxLines: 1,
+                    controller: _controllerTitlePlace,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: TextInput(
+                    hintText: 'Description',
+                    inputType: TextInputType.multiline,
+                    maxLines: 4,
+                    controller: _controllerDescriptionPlace,
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
